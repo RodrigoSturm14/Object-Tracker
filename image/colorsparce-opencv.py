f16fe70ae1d369c626c72097ebad69252b10a3ef
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-13 10:58:57 -0300

    Visualizacion de imagen en HSV

--- image/colorsparce-opencv.py
+++ image/colorsparce-opencv.py
@@ -4,9 +4,9 @@ import os
 img = cv2.imread(os.path.join('.', 'assets', 'zer_3.jpg'))
 resized_img = cv2.resize(img, (1040, 586))
 
-img_gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
-print(img_gray.shape)
+img_hsv = cv2.cvtColor(resized_img, cv2.COLOR_BGR2HSV)
+print(img_hsv.shape)
 
 cv2.imshow('imagen bgr', resized_img)
-cv2.imshow('imagen gray', img_gray)
+cv2.imshow('imagen gray', img_hsv)
 cv2.waitKey(0)
